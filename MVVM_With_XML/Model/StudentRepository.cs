f0c0ed300299205e8420b20002a78eee9ce5bf56
lHatePeople <sergeybednyuk@gmail.com>
2017-08-26 00:39:31 +0200

    XML to repository + XAML bindings to repository

---
+++ MVVM_With_XML/Model/StudentRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MVVM_With_XML.Model
+{
+    public class StudentRepository
+    {
+     
+        private static ObservableCollection<Student> _students = new ObservableCollection<Student>();
+
+        //public ObservableCollection<Student> Students
+        //{
+        //    get { return _students; }
+        //    set { _students = value; }
+        //}
+
+        static StudentRepository()
+        {
+            //@"d:\itstep\WPF\MVVM\MVVM_With_XML\Students.xml"
+
+            XDocument _xDoc = XDocument.Load(@"d:\itstep\WPF\MVVM\MVVM_With_XML\Students.xml");
+
+            var data = from node in _xDoc.Element("Students").Elements("Student")
+                       select node;
+
+            foreach (XElement node in data)
+            {
+                Student student = new Student();
+                student.Id = Convert.ToInt32(node.Attribute("Id").Value);
+                student.FirstName = node.Element("FirstName").Value;
+                student.LastName = node.Element("Last").Value;
+                student.Age = Convert.ToInt32(node.Element("Age").Value);
+                student.Gender = Convert.ToInt32(node.Element("Gender").Value);
+
+                _students.Add(student);
+            }
+        }
+
+        public static ObservableCollection<Student> GetAllCollection()
+        {
+            return _students;
+        }
+
+        public static void AddToCollection()
+        {
+            
+        }
+    }
+}
---
+++ MVVM_With_XML/ViewModel/MainwindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using MVVM_With_XML.Model;
+
+namespace MVVM_With_XML.ViewModel
+{
+    public class MainwindowViewModel : ViewModelBase
+    {
+        //private StudentRepository _studentRepository = new StudentRepository();
+
+        private ObservableCollection<Student> _students;
+        public ObservableCollection<Student> Students
+        {
+            get
+            {
+                if (_students == null)
+                {
+                    _students = StudentRepository.GetAllCollection();
+                }
+                return _students;
+            }
+            set
+            {
+                _students = value;
+                RaisePropertyChanged("Students");
+            }
+        }
+
+        
+    }
+}
